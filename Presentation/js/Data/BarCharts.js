bfd77450ae67d1b073e1ce2fe1c9795da2717989
Mirope Hu <miropehu@gmail.com>
2019-07-12 04:50:23 +0800

    Update presentation

--- Presentation/js/Data/BarCharts.js
+++ Presentation/js/Data/BarCharts.js
@@ -1,7 +1,16 @@
-var YBarChart_option1 = {
+var YBarChart_option0 = {
     title: {
-        text: '因子影响性',
-        subtext: 'reference here'
+        text: '三个产业的因子重要性',
+        subtext: '数据来源于193个上市公司',
+        textStyle: {
+            fontSize: 40,
+            lineHeight: 50
+        },
+        subtextStyle: {
+            fontSize: 25,
+            color: 'rgba(0,0,0,1)'
+        },
+        x: 'center'
     },
     tooltip: {
         trigger: 'axis',
@@ -9,51 +18,119 @@ var YBarChart_option1 = {
             type: 'shadow'
         }
     },
+    textStyle: {
+        fontSize: 20
+    },
     legend: {
-        data: ['三个行业', '交通运输', '电子制造', '软件信息'],
-        selected: {
-            '三个行业': false,
-            '交通运输': false,
-            '电子制造': false,
-            '软件信息': false
-        }
+        top: '12%',
     },
     grid: {
         left: '3%',
         right: '4%',
         bottom: '3%',
+        top: '15%',
+        height: '600',
         containLabel: true
     },
     xAxis: {
         type: 'value',
-        boundaryGap: [0, 0.01]
+        boundaryGap: [0, 0.01],
+        axisLabel: {
+            textStyle: {
+                fontSize: 18
+            }
+        }
     },
     yAxis: {
         type: 'category',
-        data: ['factor', 'factor', 'factor', 'factor', 'factor',
-            'factor', 'factor', 'factor', 'factor', 'factor']
+        data: ['资产总计相对年初增长率(TotastYTDgrrt)', '正面舆论比率(Positive)', '股东权益周转率(Equart)', '存货周转率(Invtrtrrat)', '固定资产周转率(Fixassrat)',
+            '流动资产周转率(Currat)', 'Logo绿色程度(Green)',
+            '经营现金净流量(NOCF)', '净利润(Netprf)', '息税前利润(EBIT)',
+            '每股现金及现金等价物余额(CCEPS)', '息税折旧摊销前利润(EBITDA)'],
+        axisLabel: {
+            textStyle: {
+                fontSize: 18
+            }
+        }
     },
     series: [
         {
-            name: '三个行业',
             type: 'bar',
-            data: [5, 6, 10, 11, 12, 15, 19, 24, 35, 60]
+            data: [1.51, 2.31, 2.37, 2.46, 2.70, 3.06, 3.43, 3.73, 4.44, 5.74, 7.79, 14.51]
+        }
+    ]
+};
+
+var YBarChart_option1 = {
+    title: {
+        text: '三个产业的因子重要性对比',
+        textStyle: {
+            fontSize: 40,
+            lineHeight: 50
+        },
+        x: 'center'
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    textStyle: {
+        fontSize: 20
+    },
+    legend: {
+        data: ['软件信息', '交通运输', '电子制造'],
+        top: '12%',
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        top: '20%',
+        height: '600',
+        containLabel: true
+    },
+    xAxis: {
+        type: 'value',
+        boundaryGap: [0, 0.01],
+        axisLabel: {
+            textStyle: {
+                fontSize: 18
+            }
+        }
+    },
+    yAxis: {
+        type: 'category',
+        data: ['存货周转率(Invtrtrrat)', '应收账款周转率(ARTrat)', '负面舆情率(Na)',
+            '负面/中性舆情比率(Na/Ne)', '每股收益(BasicEPS)',
+            '固定资产周转率(Fixassrat)', '营业周期(Opencyc)', '稀释每股收益增长率(EPSgrrt)',
+            '息税折旧摊销前利润(EBITDA)', 'Logo绿色程度(Green)'],
+        nameTextStyle: {
+            fontSize: 40,
+        },
+        axisLabel: {
+            textStyle: {
+                fontSize: 18
+            }
+        }
+    },
+    series: [
+        {
+            name: '软件信息',
+            type: 'bar',
+            data: [1.24, 1.25, 7.11, 1.27, 1.28, 8.61, 1.46, 13.02, 5.31, 1.29]
         },
         {
             name: '交通运输',
             type: 'bar',
-            data: [10, 6, 17, 23, 12, 5, 9, 14, 25, 32]
+            data: [6.60, 7.61, 2.25, 0.39, 8.16, 0.4, 21.06, 0.41, 0.44, 21.12]
         },
         {
             name: '电子制造',
             type: 'bar',
-            data: [5, 6, 7, 13, 2, 11, 8, 24, 35, 52]
+            data: [4.08, 0.93, 0.94, 4.67, 0.95, 0.96, 4.95, 0.97, 32.84, 0.98]
         },
-        {
-            name: '软件信息',
-            type: 'bar',
-            data: [15, 9, 10, 13, 7, 35, 39, 24, 34, 11]
-        }
     ]
 };
 
@@ -66,8 +143,10 @@ var BarChart_5_4_option = {
             lineHeight: 50
         },
         subtextStyle: {
-            fontSize: 20
+            fontSize: 25,
+            color: 'rgba(0,0,0,1)'
         },
+        x: 'center'
     },
     color: ['#3398DB'],
     tooltip: {
@@ -88,9 +167,14 @@ var BarChart_5_4_option = {
         {
             type: 'category',
             data: ['制造', '消费品', 'ICT', '金融',
-                '公司服务', '旅游度假', '零售', '交通运输', '教育', '媒体通信'],
+                '软件服务', '旅游度假', '零售', '交通运输', '教育', '媒体通信'],
             axisTick: {
                 alignWithLabel: true
+            },
+            axisLabel: {
+                textStyle: {
+                    fontSize: 18
+                }
             }
         }
     ],
@@ -100,6 +184,9 @@ var BarChart_5_4_option = {
             axisLabel: {
                 formatter: function (value) {
                     return value + '%'
+                },
+                textStyle: {
+                    fontSize: 18
                 }
             }
         }
--- Presentation/js/Data/RadarCharts.js
+++ Presentation/js/Data/RadarCharts.js
@@ -1,27 +1,44 @@
-var RadarChart_option1 = {
+var RadarChart_option2_beixun = {
     title: {
-        text: '四个维度的公司画像模型',
-        subtext: 'reference here'
+        text: '北讯25个维度的画像',
     },
     tooltip: {
         trigger: 'axis'
     },
     legend: {
-        data: ['某公司'],
-        selected: {
-            '某公司': false
-        }
+        data: ['北讯集团股份有限公司'],
     },
     radar: [
         {
             indicator: [
-                { text: '环境', max: 100 },
-                { text: '产业', max: 100 },
-                { text: '财务', max: 100 },
-                { text: '非财务', max: 100 }
+                { text: '经济气候', max: 1, color: 'red' },
+                { text: '市场开放度', max: 1, color: 'red' },
+                { text: '监管合规', max: 1, color: 'red' },
+                { text: '法律法制', max: 1, color: 'red' },
+                { text: '税务税负', max: 1, color: 'red' },
+                { text: '数字行业人才分布', max: 1, color: '#43B02A' },
+                { text: '创新机构行业分布占比', max: 1, color: '#43B02A' },
+                { text: '高水平人才行业分布', max: 1, color: '#43B02A' },
+                { text: '劳动力行业分布', max: 1, color: '#43B02A' },
+                { text: '是否为周期性行业', max: 1, color: '#43B02A' },
+                { text: '行业数字化程度', max: 1, color: '#43B02A' },
+                { text: '行业集中度', max: 1, color: '#43B02A' },
+                { text: '现金流量', max: 1, color: '#00A3E0' },
+                { text: '收益质量', max: 1, color: '#00A3E0' },
+                { text: '资本结构', max: 1, color: '#00A3E0' },
+                { text: '偿债能力', max: 1, color: '#00A3E0' },
+                { text: '盈利能力', max: 1, color: '#00A3E0' },
+                { text: '股票指标', max: 1, color: '#00A3E0' },
+                { text: '主营业务', max: 1, color: '#ED8B00' },
+                { text: '社会舆论', max: 1, color: '#ED8B00' },
+                { text: '企业文化', max: 1, color: '#ED8B00' },
+                { text: '运营能力', max: 1, color: '#ED8B00' },
+                { text: '欺诈历史', max: 1, color: '#ED8B00' },
+                { text: '成长能力', max: 1, color: '#ED8B00' },
+                { text: '股权结构', max: 1, color: '#ED8B00' },
             ],
             center: ['50%', '50%'],
-            radius: 240,
+            radius: 200,
             startAngle: 90,
             splitNumber: 5,
             shape: 'circle',
@@ -30,14 +47,114 @@ var RadarChart_option1 = {
                 textStyle: {
                     color: '#86BC25'
                 },
-                fontSize: 20
+                fontSize: 15
             },
             splitArea: {
                 areaStyle: {
-                    color:
-                        ['rgba(134, 188, 37, 0.1)', 'rgba(134, 188, 37, 0.2)',
-                            'rgba(134, 188, 37, 0.3)', 'rgba(134, 188, 37, 0.5)',
-                            'rgba(134, 188, 37, 0.7)'],
+                    shadowColor: 'rgba(0, 0, 0, 0.3)',
+                    shadowBlur: 10
+                }
+            },
+            axisLine: {
+                lineStyle: {
+                    color: 'rgba(4, 106, 56, 0.5)'
+                }
+            },
+            splitLine: {
+                lineStyle: {
+                    color: 'rgba(4, 106, 56, 0.5)'
+                }
+            },
+        }
+    ],
+    series: [
+        {
+            name: '雷达图',
+            type: 'radar',
+            tooltip: {
+                trigger: 'item'
+            },
+            itemStyle: {
+                emphasis: {
+                    lineStyle: {
+                        width: 4
+                    }
+                }
+            },
+            data: [
+                {
+                    value: [
+                        0.87, 0.83, 0.59, 0.78, 0.57, //外部环境
+                        0.399, 0.06, 0.127, 0.029, 1, 0.845, 0.329, //产业层
+                        0.332, 0.662, 0.516, 0.164, 0.477, 0.436, //内部财务层
+                        0.125, 0.357, 0.297, 0.039, 0, 0.356, 0.318 //内部非财务层
+                    ],
+                    name: '北讯集团股份有限公司',
+                    areaStyle: {
+                        normal: {
+                            color: 'rgba(4, 106, 56, 0.5)'
+                        }
+                    }
+                }
+            ],
+            color: 'red',
+        }
+    ]
+};
+
+var RadarChart_option2_shunfeng = {
+    title: {
+        text: '顺丰25个维度的画像',
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['顺丰控股股份有限公司'],
+    },
+    radar: [
+        {
+            indicator: [
+                { text: '经济气候', max: 1, color: 'red' },
+                { text: '市场开放度', max: 1, color: 'red' },
+                { text: '监管合规', max: 1, color: 'red' },
+                { text: '法律法制', max: 1, color: 'red' },
+                { text: '税务税负', max: 1, color: 'red' },
+                { text: '数字行业人才分布', max: 1, color: '#43B02A' },
+                { text: '创新机构行业分布占比', max: 1, color: '#43B02A' },
+                { text: '高水平人才行业分布', max: 1, color: '#43B02A' },
+                { text: '劳动力行业分布', max: 1, color: '#43B02A' },
+                { text: '是否为周期性行业', max: 1, color: '#43B02A' },
+                { text: '行业数字化程度', max: 1, color: '#43B02A' },
+                { text: '行业集中度', max: 1, color: '#43B02A' },
+                { text: '现金流量', max: 1, color: '#00A3E0' },
+                { text: '收益质量', max: 1, color: '#00A3E0' },
+                { text: '资本结构', max: 1, color: '#00A3E0' },
+                { text: '偿债能力', max: 1, color: '#00A3E0' },
+                { text: '盈利能力', max: 1, color: '#00A3E0' },
+                { text: '股票指标', max: 1, color: '#00A3E0' },
+                { text: '主营业务', max: 1, color: '#ED8B00' },
+                { text: '社会舆论', max: 1, color: '#ED8B00' },
+                { text: '企业文化', max: 1, color: '#ED8B00' },
+                { text: '运营能力', max: 1, color: '#ED8B00' },
+                { text: '欺诈历史', max: 1, color: '#ED8B00' },
+                { text: '成长能力', max: 1, color: '#ED8B00' },
+                { text: '股权结构', max: 1, color: '#ED8B00' },
+            ],
+            center: ['50%', '50%'],
+            radius: 200,
+            startAngle: 90,
+            splitNumber: 5,
+            shape: 'circle',
+            name: {
+                formatter: '{value}',
+                textStyle: {
+                    color: '#86BC25'
+                },
+                fontSize: 15
+            },
+            splitArea: {
+                areaStyle: {
                     shadowColor: 'rgba(0, 0, 0, 0.3)',
                     shadowBlur: 10
                 }
@@ -70,7 +187,202 @@ var RadarChart_option1 = {
             },
             data: [
                 {
-                    value: [60, 45, 80, 80, 1500],
+                    value: [
+                        0.87, 0.83, 0.59, 0.78, 0.57, //外部环境
+                        0.022, 0.032, 0.044, 0.048, 0, 0.136, 0.399, //产业层
+                        0.356, 0.705, 0.424, 0.254, 0.543, 0.705, //内部财务层
+                        0.125, 0.686, 0, 0.167, 0, 0.457, 0.509 //内部非财务层
+                    ],
+                    name: '顺丰控股股份有限公司',
+                    areaStyle: {
+                        normal: {
+                            color: 'rgba(4, 106, 56, 0.5)'
+                        }
+                    }
+                }
+            ],
+            color: 'black'
+        }
+    ]
+};
+
+var RadarChart_option1 = {
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['公司A', '公司B'],
+        selected: {
+            '公司A': false,
+            '公司B': false
+        },
+    },
+
+    radar: [
+        {
+            indicator: [
+                { text: '环境', max: 1 },
+                { text: '产业', max: 1 },
+                { text: '财务', max: 1 },
+                { text: '非财务', max: 1 }
+            ],
+            center: ['50%', '50%'],
+            radius: 240,
+            startAngle: 90,
+            splitNumber: 5,
+            shape: 'circle',
+
+            name: {
+                formatter: '{value}',
+                textStyle: {
+                    color: '#86BC25'
+                },
+                fontSize: 30
+            },
+            splitArea: {
+                areaStyle: {
+                    shadowColor: 'rgba(0, 0, 0, 0.3)',
+                    shadowBlur: 10
+                }
+            },
+            axisLine: {
+                lineStyle: {
+                    color: 'rgba(4, 106, 56, 0.5)'
+                }
+            },
+            splitLine: {
+                lineStyle: {
+                    color: 'rgba(4, 106, 56, 0.5)'
+                }
+            }
+        }
+    ],
+    series: [
+        {
+            name: '雷达图',
+            type: 'radar',
+            tooltip: {
+                trigger: 'item'
+            },
+            itemStyle: {
+                emphasis: {
+                    lineStyle: {
+                        width: 4
+                    }
+                }
+            },
+            data: [
+                {
+                    value: [0.728, 0.59, 0.431, 0.213],
+                    name: '公司A',
+                    areaStyle: {
+                        normal: {
+                            color: 'rgba(4, 106, 56, 0.5)'
+                        }
+                    },
+                },
+                {
+                    value: [0.728, 0.097, 0.62, 0.78],
+                    name: '公司B',
+                    areaStyle: {
+                        normal: {
+                            color: 'rgba(4, 106, 56, 0.5)'
+                        }
+                    },
+                }
+            ],
+        }
+    ]
+};
+
+var RadarChart_option2 = {
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['某公司'],
+    },
+    radar: [
+        {
+            indicator: [
+                { text: '经济气候', max: 1, color: 'red' },
+                { text: '市场开放度', max: 1, color: 'red' },
+                { text: '监管合规', max: 1, color: 'red' },
+                { text: '法律法制', max: 1, color: 'red' },
+                { text: '税务税负', max: 1, color: 'red' },
+                { text: '数字行业人才分布', max: 1, color: '#43B02A' },
+                { text: '创新机构行业分布占比', max: 1, color: '#43B02A' },
+                { text: '高水平人才行业分布', max: 1, color: '#43B02A' },
+                { text: '劳动力行业分布', max: 1, color: '#43B02A' },
+                { text: '是否为周期性行业', max: 1, color: '#43B02A' },
+                { text: '行业数字化程度', max: 1, color: '#43B02A' },
+                { text: '行业集中度', max: 1, color: '#43B02A' },
+                { text: '现金流量', max: 1, color: '#00A3E0' },
+                { text: '收益质量', max: 1, color: '#00A3E0' },
+                { text: '资本结构', max: 1, color: '#00A3E0' },
+                { text: '偿债能力', max: 1, color: '#00A3E0' },
+                { text: '盈利能力', max: 1, color: '#00A3E0' },
+                { text: '股票指标', max: 1, color: '#00A3E0' },
+                { text: '主营业务', max: 1, color: '#ED8B00' },
+                { text: '社会舆论', max: 1, color: '#ED8B00' },
+                { text: '企业文化', max: 1, color: '#ED8B00' },
+                { text: '运营能力', max: 1, color: '#ED8B00' },
+                { text: '欺诈历史', max: 1, color: '#ED8B00' },
+                { text: '成长能力', max: 1, color: '#ED8B00' },
+                { text: '股权结构', max: 1, color: '#ED8B00' },
+            ],
+            center: ['50%', '50%'],
+            radius: 280,
+            startAngle: 90,
+            splitNumber: 5,
+            shape: 'circle',
+            name: {
+                formatter: '{value}',
+                textStyle: {
+                    color: '#86BC25'
+                },
+                fontSize: 20
+            },
+            splitArea: {
+                areaStyle: {
+                    shadowColor: 'rgba(0, 0, 0, 0.3)',
+                    shadowBlur: 10
+                }
+            },
+            axisLine: {
+                lineStyle: {
+                    color: 'rgba(4, 106, 56, 0.5)'
+                }
+            },
+            splitLine: {
+                lineStyle: {
+                    color: 'rgba(4, 106, 56, 0.5)'
+                }
+            },
+        }
+    ],
+    series: [
+        {
+            name: '雷达图',
+            type: 'radar',
+            tooltip: {
+                trigger: 'item'
+            },
+            itemStyle: {
+                emphasis: {
+                    lineStyle: {
+                        width: 4
+                    }
+                }
+            },
+            data: [
+                {
+                    value: [
+                        0.5, 0.6, 0.5, 0.7, 0.27, //外部环境
+                        0.4, 0.6, 0.7, 0.3, 0.4, 0.8, 0.3, //产业层
+                        0.3, 0.6, 0.5, 0.1, 0.4, 0.6, //内部财务层
+                        0.5, 0.7, 0.9, 0.2, 0.3, 0.4, 0.8 //内部非财务层
+                    ],
                     name: '某公司',
                     areaStyle: {
                         normal: {
@@ -78,7 +390,8 @@ var RadarChart_option1 = {
                         }
                     }
                 }
-            ]
+            ],
+            color: 'darkblue',
         }
     ]
 };
